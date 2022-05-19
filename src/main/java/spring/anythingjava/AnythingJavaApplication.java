7217ca017365ea2e1d710551c54f27f12025f8cd
mwchoen <mwcheon@cudo.co.kr>
2022-05-19 11:37:19 +0900

    first commit

---
+++ src/main/java/spring/anythingjava/AnythingJavaApplication.java
@@ -0,0 +1,13 @@
+package spring.anythingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnythingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnythingJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/anythingjava/AnythingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.anythingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnythingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
